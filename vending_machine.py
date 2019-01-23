d21e5a8a34fd393bef80f7670df09a9adf69a83f
tom1mol <tomasomaolmhuaidh@gmail.com>
2019-01-23 22:42:34 +0000

    challenge 1

--- vending_machine.py
+++ vending_machine.py
@@ -1,15 +1,22 @@
 from byotest import *                #from byotest import everything
 
-def get_change(amount):
-    if amount == 0:                 #amount = amount of money we ned to provide change for
-        return []
-     
-    if amount in [100, 50, 20, 10, 5, 2, 1]:        #if statement protects existing tests
-        return [amount]
+usd_coins = [100, 50, 25, 10, 5, 1]
+eur_coins = [100, 50, 20, 10, 5, 2, 1]
+
+def get_change(amount, coins=eur_coins): # equal sign means will default to eur_coins unless we supply argument(usd)
+    """
+    Takes the payment amount and returns the change
+    `amount` the amount of money that we need to provide change for
+    `coins` is the set of coins that we need to get change for (i.e. the set
+        of available coins)
+    Returns a list of coin values
+    """
+   
     
     change = []
-    for coin in [100, 50, 20, 10, 5, 2, 1]:
-        if coin <= amount:                  #if coin value <= value we passed in..
+    for coin in coins:
+        while coin <= amount:                  #if coin value <= value we passed in..
+                                            #while coin <= amount..keep adding until it isnt. then move on or rtn change
             amount -= coin                  #deduct amount of coin from amount we sent in
             change.append(coin)             #add that onto our change
     return change                           #return change list
@@ -24,5 +31,7 @@ test_are_equal(get_change(50), [50])
 test_are_equal(get_change(100), [100])
 test_are_equal(get_change(3),[2,1])
 test_are_equal(get_change(7),[5,2])
+test_are_equal(get_change(9),[5,2,2])
+test_are_equal(get_change(35, usd_coins),[25,10])
 
 print("All tests pass!")
